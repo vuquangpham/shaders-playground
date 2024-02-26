286f9d3ce0adb2bbe06b2a9b2a75653d1c77fbae
vuquangpham <vuquangpham2909@gmail.com>
2024-02-27 00:37:41 +0700

    feat: initial

--- core/routes/404.js
+++ core/routes/404.js
@@ -1,9 +1,10 @@
-const errorHandler = (error, _, response) => {
+/* eslint-disable no-unused-vars */
+const errorHandler = (error, _, response, _next) => {
   response.render("pages/404", {
     title: "OOPS! \n" + "404 PAGE NOT FOUND",
     message:
       "The page you’re looking for does not exist or has been removed.\n" +
-      "You can proceed to our <a href=\"/\">home page</a>.",
+      'You can proceed to our <a href="/">home page</a>.',
   });
 };
 
--- pages.js
+++ pages.js
@@ -1,12 +1,18 @@
 const { createPagesPrototype } = require("./core/utils/utils");
 module.exports = createPagesPrototype([
-  // {
-  //   title: "WebGL Fundamentals",
-  //   base: "webgl",
-  //   pages: [
-  //     {
-  //       title: "Drawing a single point",
-  //     },
-  //   ],
-  // },
+  {
+    title: "The Book of Shaders",
+    base: "book-of-shaders",
+    pages: [],
+  },
+  {
+    title: "Three.js Journey",
+    base: "threejs-journey",
+    pages: [],
+  },
+  {
+    title: "Yuri Math",
+    base: "yuri-math",
+    pages: [],
+  },
 ]);
